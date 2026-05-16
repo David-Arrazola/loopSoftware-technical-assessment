e703f748b69a389cf36f602b8575cfbfa197e2b3
David Arrazola <arrazola.dvd@gmail.com>
2026-05-16 09:24:40 -0400

    FIX error, locator was matching to several elements. CHANGE to matching if h1 of page is called; web application

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,9 +1,18 @@
 import { test, expect } from "@playwright/test";
+import testCases from "./testCases";
 
-test("Successful login", async ({ page }) => {
-  await page.goto("https://animated-gingersnap-8cf7f2.netlify.app/");
-  await page.fill(`input[type="text"]`, "admin");
-  await page.fill(`input[type="password"]`, "password123");
-  await page.click('button[type="submit"]');
-  await expect(page.locator("text=Web Application")).toBeVisible();
-});
+for (const currCase of testCases) {
+  test(`Verify: ${currCase.task}`, async ({ page }) => {
+    //logic for logging in per every test case
+    await page.goto("https://animated-gingersnap-8cf7f2.netlify.app/");
+    await page.fill(`input[type="text"]`, "admin");
+    await page.fill(`input[type="password"]`, "password123");
+    await page.click('button[type="submit"]');
+    await expect(
+      page.locator("h1", { hasText: "Web Application" }),
+    ).toBeVisible();
+
+    //
+    await page.click(`text=${currCase.app}`);
+  });
+}
---
+++ tests/testCases.js
@@ -0,0 +1,12 @@
+import test from "node:test";
+
+const testCases = [
+  {
+    app: "Web Application",
+    task: "Implemented user authentication",
+    column: "To Do",
+    tags: ["Feature", "High Priority"],
+  },
+];
+
+export default testCases;
